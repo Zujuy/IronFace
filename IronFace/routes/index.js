f53ed464cbf524c72c8fb439e6f463f8f34b1295
Itza Zujuy Blanco <39780663+Zujuy@users.noreply.github.com>
2019-11-27 14:21:03 -0600

    vistas profile, feeds y edit

--- IronFace/routes/index.js
+++ IronFace/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router  = express.Router()
 const Post = require('../models/Post')
+const { ensureLoggedIn } = require("connect-ensure-login")
 
 router.get('/', (req, res, next) => {
   Post.find()
   .then(posts => {
-    res.render('index', { title: 'IronFace', posts })
+    res.render('index', { user: req.user, title: 'IronFace', posts })
   })
   .catch(err => {
     console.log(err)
