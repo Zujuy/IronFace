0c7e643e0f85c6a097fb111c9e91b9d3f9a750d9
RicardoSalas17 <ricardosalas.1716@gmail.com>
2019-11-29 13:28:08 -0600

    ultimo commit de routes

--- routes/IBRoutes.js
+++ routes/IBRoutes.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const {
   feedsGet,
   postPost,
-  // commentPost,
+  commentPost,
   editUserGet,
   editUserPost,
   deleteUserPost,
   commentsGet,
-
-
+  eventGet,
+  alleventGet,
+  allusersGet,
+  profileDetailGet
 } = require("../controllers/staff.controllers");
 const { profileGet } = require("../controllers/auth.controllers");
 const upload = require("../config/cloudinary");
@@ -18,9 +20,27 @@ router.get("/comments/:id", commentsGet)
 router.get("/profile", profileGet);
 // router.post("/profile", upload.single("photoURL"), profilePost);
 router.post("/post", upload.single("photoURL"), postPost);
-// router.post("/comment", upload.single("photoURL"), commentPost);
+router.post("/comment", upload.single("photoURL"), commentPost);
+
+
 router.get("/edit", editUserGet);
-router.post("/edit", upload.single("photoURL"), editUserPost);
+router.post("/edit", editUserPost);
+
+router.get("/event", eventGet);
+
+
+router.get("/allUsers", allusersGet);
+
+
+router.get("/allevents", alleventGet);
+
+router.post("/delete", deleteUserPost);
+
+router.get("/ironhacker/:id", profileDetailGet);
+
+
+const User = require("../models/User");
+
 
 
 
